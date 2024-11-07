7a5d1b4c0ebe20ea4e3c5b6f4291e2e7e6be39c1
ehorn36 <62220173+ehorn36@users.noreply.github.com>
2024-11-07 13:26:30 -0500

    Pushing to GH

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # CS361_Microservice_A
+
+### <ins>Setting-up the communication pipe:</ins>
+
+* microservice.py communicates with clients using ZeroMQ and ("tcp://localhost:5555")
+
+* The microservice expects incoming requests to be a formatted JSON string:
+####
+    sample_json_string = '''{
+    "labels": {
+        "title": "Math Progress",
+        "x-axis": "Date",
+        "y-axis": "DCPM"
+    },
+    "x-axis-data": [
+        "10/1",
+        "10/8",
+        "10/15"
+    ],
+    "y-axis-data": [
+        "13",
+        "16",
+        "15"
+    ]
+    }'''
+
+#### <ins>Import these modules into Python:</ins>
+    import zmq                          # for ZeroMQ library.
+    import io                           # To store files into an image buffer.
+    import base64                       # To decode base64 text.
+    from PIL import Image               # To view images.
+ 
+#### <ins>Sending data to the microservice:</ins>
+    socket.send_json(sample_json_string)                # Using zmq library.
+
+#### <ins>Receiving data from the microservice:</ins>
+    message = socket.recv()                             # Using zmq library.
+
+#### <ins>Received data must be decoded and converted into a byte array:</ins>
+
+    byte_array = bytearray(base64.b64decode(message))   # Using base64 library.
+
+#### <ins>Move byte array into memory buffer variable so that PIL can access:</ins></ins>
+    image_buffer = io.BytesIO(byte_array)               # Using io library.
+
+#### <ins>Display image using PIL </ins></ins>
+    image = Image.open(image_buffer)                    # Using PIL library.
+    image.show()              
+
+![uml_diagram.png](uml_diagram.png)
--- microservice.py
+++ microservice.py
@@ -17,22 +17,24 @@ while True:
     #  Wait for request from client, which zmq turns into a dictionary.
     data = socket.recv_json()
     print("Received reply from server")
+    print(data)
 
     # Assigns a JSON string to a variable.
-    json = json.loads(data)
+    json_data = json.loads(data)
 
     # Assigns a JSON file to a variable.
     # json = json.loads(data)
 
-    # Extract titles from JSON file.
-    main_title = json["labels"]["title"]
-    x_axis_label = json["labels"]["x-axis"]
-    y_axis_label = json["labels"]["y-axis"]
+    # Extract titles from JSON object.
+    main_title = json_data["labels"]["title"]
+    x_axis_label = json_data["labels"]["x-axis"]
+    y_axis_label = json_data["labels"]["y-axis"]
 
-    # Extract data from JSON file.
-    x_axis_data = json["x-axis-data"]
-    y_axis_data = json["y-axis-data"]
+    # Extract data from JSON object.
+    x_axis_data = json_data["x-axis-data"]
+    y_axis_data = json_data["y-axis-data"]
 
+    # https://www.geeksforgeeks.org/matplotlib-pyplot-title-in-python/
     # Assign data to matplotlib graph.
     plt.plot(x_axis_data, y_axis_data)
 
@@ -47,19 +49,20 @@ while True:
     # Title: BytesIO For Managing Data As File Object in Python
     # Source: https://www.geeksforgeeks.org/stringio-and-bytesio-for-managing-data-as-file-object/
     # Create ByteIO buffer object.
-    graph_image = io.BytesIO()
+    graph_image_buffer = io.BytesIO()
 
     # Save image to ByteIO buffer in .png format.
-    plt.savefig(graph_image, format='png')
+    plt.savefig(graph_image_buffer, format='png')
 
     # Move buffer pointer back to beginning of the file for reading.
-    graph_image.seek(0)
+    graph_image_buffer.seek(0)
 
     # Title: ZeroMQ pyzmq send jpeg image over tcp
     # Source: https://stackoverflow.com/questions/24791633/zeromq-pyzmq-send-jpeg-image-over-tcp
     # Encode image to bytes, then encode using b64.
-    graph_image_bytes = bytearray(graph_image.read())
+    graph_image_bytes = bytearray(graph_image_buffer.read())
     graph_image_bytes_b64_string = base64.b64encode(graph_image_bytes)
 
     #  Send reply back to client via base64 string
     socket.send(graph_image_bytes_b64_string)
+    print("Sending reply.")
